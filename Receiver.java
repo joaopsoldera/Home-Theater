bd5f1963520815ced16482ecf812b1139f4b8f12
joaopsoldera <joao.markues@unifesp.br>
2026-04-14 19:44:11 -0300

    enviando

--- Receiver.java
+++ Receiver.java
@@ -1,22 +1,6 @@
 public class Receiver {
-    private String marca;
-    private int canais;
-    private double potenciaWatts;
 
-    public Receiver() {}
-
-    public Receiver(String marca, int canais, double potenciaWatts) {
-        this.marca = marca;
-        this.canais = canais;
-        this.potenciaWatts = potenciaWatts;
+    public void ligarReceiver() {
+        System.out.println("Ligando Receiver");
     }
-
-    public String getMarca() { return marca; }
-    public void setMarca(String marca) { this.marca = marca; }
-
-    public int getCanais() { return canais; }
-    public void setCanais(int canais) { this.canais = canais; }
-
-    public double getPotenciaWatts() { return potenciaWatts; }
-    public void setPotenciaWatts(double potenciaWatts) { this.potenciaWatts = potenciaWatts; }
 }
